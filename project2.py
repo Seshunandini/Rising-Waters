61cc2c1ed9e1d8bd15bf791559c814dea5658eda
Shamili <nallamallashamili@gmail.com>
2026-07-09 20:22:40 +0530

    Update project2.py
    Removed unnecessary import statements and print statements.

--- project2.py
+++ project2.py
@@ -1,13 +1,3 @@
 
 
 
-# Import required libraries
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-print("NumPy version:", np.__version__)
-print("Pandas version:", pd.__version__)
-print("Libraries imported successfully!")
